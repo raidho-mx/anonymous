ada298a831364a43c81d2c9b772700ae76f86bab
raidho-mx <miranda@raidho.mx>
2016-02-22 17:24:03 -0600

    Working basic Index

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
+<?php
+/*
+Designaholic Templates
+Reciente
+*/
+?>
+
 <?php get_header(); ?>
+
+	<div class="container spacer">
+
+		<h2>Reciente</h2><?php
+
+		// Text Ads
+		get_template_part('inc/ad_dh_dos');
+
+
+		// World's Most Basic Loop
+		if ( have_posts() ) : ?>
+			<div class="row fluid"><?php
+				while ( have_posts() ) {
+					the_post();
+					cards();
+				} ?>
+			</div><?php
+
+			get_template_part('inc/pager');
+		endif;
+
+
+		// Img Ads
+		get_template_part('inc/patrocinadores'); ?>
+
+	</div>
+
 <?php get_footer(); ?>
