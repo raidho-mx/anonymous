2d204361e476632ec590d72740b203d0250eeea5
raidho-mx <miranda@raidho.mx>
2016-02-23 18:33:49 -0600

    :shower: Loop

--- functions.php
+++ functions.php
@@ -96,33 +96,3 @@
 				</a>
 			</div><?php
 		}
-
-
-
-
-	// Comments
-
-		function format_comment($comment, $args, $depth) {
-
-			$GLOBALS['comment'] = $comment; ?>
-
-			<li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
-
-			<div class="comment-intro">
-			<em>commented on</em>
-			<a class="comment-permalink" href="<?php echo htmlspecialchars ( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__('%1$s'), get_comment_date(), get_comment_time()) ?></a>
-			<em>by</em>
-			<?php printf(__('%s'), get_comment_author_link()) ?>
-			</div>
-
-			<?php if ($comment->comment_approved == '0') : ?>
-			<em><php _e('Your comment is awaiting moderation.') ?></em><br />
-			<?php endif; ?>
-
-			<?php comment_text(); ?>
-
-			<div class="reply">
-				<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-			</div>
-			</li><?php
-		}
