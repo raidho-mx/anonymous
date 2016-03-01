5e009bed7d193df6ebbb0c9a5969fe0e00329151
raidho-mx <miranda@raidho.mx>
2016-03-01 11:27:27 -0600

    Tag Archive clone of primitive category

---
+++ page-archivo.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Designaholic Templates
+Categorías
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div class="container spacer">
+
+		<h2><?php the_title(); ?></h2><?php
+
+
+		// Text Ads
+		get_template_part('inc/ad_dh_dos');
+
+
+		$exCats = get_field('exclude_categories');
+		$args = array(
+			'orderby' => 'id',
+			'hide_empty' => 1,
+			'exclude' => $exCats
+		);
+		$categories = get_categories($args); ?>
+
+		<div class="row fluid loop_cats">
+			<div class="row fluid"><?php
+			foreach ($categories as $cat) {
+
+				$args2 = array("category" => $cat->cat_ID, 'posts_per_page' => 1);
+				$posts = get_posts($args2); ?>
+
+				<div class="four columns articulo">
+					<a href="<?php echo get_category_link( $cat->cat_ID ); ?>"> <?php
+						foreach($posts as $post) {
+							echo get_the_post_thumbnail($post->ID, 'large');
+						} ?>
+						<h3><?php echo $cat->name ?></h3>
+					</a>
+				</div><?php
+
+			} ?>
+			</div>
+		</div><?php
+
+
+		// Img Ads
+		patrocinadores(1); ?>
+
+	</div>
+
+<?php get_footer(); ?>
