0593ed99c634d443ec5dce76e795c2e89091f99d
raidho-mx <miranda@raidho.mx>
2016-03-01 18:24:34 -0600

    Added IMG fallback

--- page-categorias.php
+++ page-categorias.php
@@ -28,27 +28,28 @@ Categorías
 			<div class="row fluid"><?php
 			foreach ($categories as $cat) {
 
-				$args2 = array("category" => $cat->cat_ID, 'posts_per_page' => 1);
-				$posts = get_posts($args2);
-
 
 				// get last post's Img
+				$args2 = array("category" => $cat->cat_ID, 'posts_per_page' => 1);
+				$posts = get_posts($args2);
 				foreach($posts as $post) {
 					$lastPostImg = get_the_post_thumbnail_url($post->ID, 'large');
 				}
+
 
 				// get category img
 				$catImg = get_field('img', 'category_'.$cat->cat_ID);
 
 
-				// if has:  1 last post.  2 Cat image.  3 placeholder img
-				if($lastPostImg) {
-					$mainImg = $lastPostImg;
-				} elseif($catImg) {
+				// if has:  1 Cat image.  2 last post.  3 placeholder img
+				if($catImg) {
 					$catImgUrl = wp_get_attachment_image_src($catImg['ID'], 'large');
 					$mainImg = $catImgUrl[0];
+				} elseif($lastPostImg) {
+					$mainImg = $lastPostImg;
 				} else {
-					$mainImg = 'http://40.media.tumblr.com/5a64f8e10906552642483a9f0645104f/tumblr_o1ac9dY13u1ujxyzao1_1280.jpg';
+					$fallback = get_field('fallback_img', 'option');
+					$mainImg = $fallback['sizes']['large'];
 				} ?>
 
 				<div class="four columns articulo">
