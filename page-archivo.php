16aed2f7872b6b4b92828a749ab0485dbf343367
raidho-mx <miranda@raidho.mx>
2016-03-01 18:22:42 -0600

    Archivo de Tags

--- page-archivo.php
+++ page-archivo.php
@@ -9,39 +9,66 @@ Categorías
 
 	<div class="container spacer">
 
-		<h2><?php the_title(); ?></h2><?php
+		<h2><?php
+		if(get_field('or_title')) {
+			the_field('or_title');
+		} else {
+			the_title();
+		} ?></h2><?php
 
 
 		// Text Ads
 		get_template_part('inc/ad_dh_dos');
 
 
-		$exCats = get_field('exclude_categories');
+		$inTags = get_field('include_tags');
 		$args = array(
 			'orderby' => 'id',
 			'hide_empty' => 1,
-			'exclude' => $exCats
+			'include' => $inTags
 		);
-		$categories = get_categories($args); ?>
+		$tags = get_tags( $args ); ?>
 
-		<div class="row fluid loop_cats">
-			<div class="row fluid"><?php
-			foreach ($categories as $cat) {
+		<div class="row fluid"><?php
 
-				$args2 = array("category" => $cat->cat_ID, 'posts_per_page' => 1);
-				$posts = get_posts($args2); ?>
+			foreach ($tags as $tag) {
 
-				<div class="four columns articulo">
-					<a href="<?php echo get_category_link( $cat->cat_ID ); ?>"> <?php
-						foreach($posts as $post) {
-							echo get_the_post_thumbnail($post->ID, 'large');
-						} ?>
-						<h3><?php echo $cat->name ?></h3>
+
+				// get last post's Img
+				$args2 = array('tag' => $tag->slug, 'posts_per_page' => 1);
+				$posts = get_posts($args2);
+				$i = 1;
+				foreach($posts as $post) {
+					$lastPostImg = get_the_post_thumbnail_url($post->ID, 'large');
+				}
+
+
+				// get category img
+				$taxImg = get_field('img', 'post_tag_'.$tag->term_id);
+
+
+				// if has:  1 Cat image.  2 last post.  3 placeholder img
+				if($taxImg) {
+					$taxImgUrl = wp_get_attachment_image_src($taxImg['ID'], 'large');
+					$mainImg = $taxImgUrl[0];
+				} elseif($lastPostImg) {
+					$mainImg = $lastPostImg;
+				} else {
+					$fallback = get_field('fallback_img', 'option');
+					$mainImg = $fallback['sizes']['large'];
+				}
+
+				$tag_link = get_tag_link( $tag->term_id ); ?>
+
+				<div class="six columns articulo"><?php
+					echo '<a href='.$tag_link.' title='.$tag->name.' class='.$tag->slug.'>'; ?>
+						<img src="<?php echo $mainImg; ?>">
+						<h3><?php echo $tag->name; ?></h3>
+						<p><?php echo $tag->description; ?></p>
 					</a>
 				</div><?php
 
 			} ?>
-			</div>
 		</div><?php
 
 
