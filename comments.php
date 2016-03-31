d8b288fbb382142dcb32d91ab6753838b7e99a3b
raidho-mx <miranda@raidho.mx>
2016-03-31 11:27:08 -0600

    Updated form display format

--- comments.php
+++ comments.php
@@ -18,23 +18,42 @@ Post Comentarios
 <div class="post_comentarios fondo_gris_calido bloque_horizontal">
 	<div class="container spacer"><?php
 		if ( have_comments() ) : ?>
-		<h2><?php
+		<h3><?php
 			printf( _nx( 'Un comentario en “%2$s”', '%1$s comentarios en “%2$s”', get_comments_number(), 'Comentarios'),
 				number_format_i18n( get_comments_number() ), get_the_title() ); ?>
-		</h2><?php
+		</h3><?php
 		endif; ?>
 
 
 		<div class="row">
 
-			<div class="five columns">
-				<?php comment_form(); ?>
+			<div class="five columns"><?php
+
+				$comments_args = array( 'fields' => apply_filters( 'comment_form_default_fields', array(
+						'author' =>	'<input id="author" name="author" type="text" value="' .
+									esc_attr( $commenter['comment_author'] ) . '" placeholder="' .
+									__( 'Tu Nombre' ) . ' ' . ( $req ? ' *' : '' ) . '" size="30"' . $aria_req . ' />',
+						'email' =>	'<input id="email" name="email" type="email" value="' .
+									esc_attr( $commenter['comment_author'] ) . '" placeholder="' .
+									__( 'Tu Correo' ) . ' ' . ( $req ? ' *' : '' ) . '" size="30"' . $aria_req . ' />',
+						'url'    => '' ) ),
+						'label_submit' => 'Comentar',
+						'title_reply' => '',
+						'title_reply_to' => '',
+						'title_reply_before' => '',
+						'title_reply_after' => '',
+						'comment_notes_after' => '',
+						'comment_field' => '<textarea id="comment" name="comment" placeholder="' . _x( 'Tus comentarios', 'noun' ) . '" aria-required="true"></textarea>',
+				);
+
+				comment_form($comments_args); ?>
 			</div><?php
 
 
 
 			if ( have_comments() ) : ?>
 			<div class="six columns offset-by-one">
+				<ul class="comentarios_listado">
 				<?php /*
 				<ul class="comentarios_listado">
 					<li>
@@ -51,6 +70,7 @@ Post Comentarios
 						'avatar_size' => 50,
 					) );
 					?>
+				</ul>
 			</div><?php
 			else :
 				echo '<div class="six columns offset-by-one">No hay comentarios.</div>';
