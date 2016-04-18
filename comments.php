93dc2ce6ef53172e7170bdeb7fdbfa5ae9d1ac04
raidho-mx <miranda@raidho.mx>
2016-04-18 18:39:34 -0500

    Hide comments when unavailable.

--- comments.php
+++ comments.php
@@ -2,34 +2,33 @@
 /*
 Designaholic Templates
 Post Comentarios
-* /
-?>
-
-<!-- Comentarios -->
+*/
 
 
-
-</div> */ ?>
-
-<?php if ( post_password_required() ) {
+if ( post_password_required() ) {
 	return;
-} ?>
+}
 
+
+if ( comments_open() ) : ?>
 <div class="post_comentarios fondo_gris_calido bloque_horizontal">
-	<div class="container spacer"><?php
-		if ( have_comments() ) : ?>
+	<div class="container spacer">
 		<h3><?php
-			printf( _nx( 'Un comentario en “%2$s”', '%1$s comentarios en “%2$s”', get_comments_number(), 'Comentarios'),
-				number_format_i18n( get_comments_number() ), get_the_title() ); ?>
-		</h3><?php
-		endif; ?>
+
+			if(have_comments()) {
+				printf( _nx( 'Un comentario en “%2$s”', '%1$s comentarios en “%2$s”', get_comments_number(), 'Comentarios'),
+				number_format_i18n( get_comments_number() ), get_the_title() );
+			} else {
+				echo 'Tienes algo que decir de: "'. get_the_title(). '"';
+			} ?>
+		</h3>
 
 
 		<div class="row">
-
 			<div class="five columns"><?php
 
-				$comments_args = array( 'fields' => apply_filters( 'comment_form_default_fields', array(
+				$comments_args = array(
+					'fields' => apply_filters( 'comment_form_default_fields', array(
 						'author' =>	'<input id="author" name="author" type="text" value="' .
 									esc_attr( $commenter['comment_author'] ) . '" placeholder="' .
 									__( 'Tu Nombre' ) . ' ' . ( $req ? ' *' : '' ) . '" size="30"' . $aria_req . ' />',
@@ -44,44 +43,28 @@ Post Comentarios
 						'title_reply_after' => '',
 						'comment_notes_after' => '',
 						'comment_field' => '<textarea id="comment" name="comment" placeholder="' . _x( 'Tus comentarios', 'noun' ) . '" aria-required="true"></textarea>',
-				);
-
+					);
 				comment_form($comments_args); ?>
-			</div><?php
+			</div>
 
 
-
-			if ( have_comments() ) : ?>
 			<div class="six columns offset-by-one">
-				<ul class="comentarios_listado">
-				<?php /*
-				<ul class="comentarios_listado">
-					<li>
-						<p><strong>Paco</strong> comentó el 31 de enero, 2016 (<strong>15:23</strong>):</p>
-						<p>El app que utilizan para visualizar su mobiliario es un tema en sí mismo, me parece que son admirables al tomar en cuenta que ponen tanto cuidado...</p>
-					</li>
-					<li>
-						<p><strong>Rogelio</strong> comentó el 31 de enero, 2016 (<strong>14:16</strong>):</p>
-						<p>Buen artículo, lo comparto.</p>
-					</li>
-				</ul>*/
-					wp_list_comments( array(
-						'short_ping'  => true,
-						'avatar_size' => 50,
-					) );
+				<ul class="comentarios_listado"><?php
+					if(have_comments()) {
+						wp_list_comments( array(
+							'short_ping'  => true,
+							'avatar_size' => 50,
+						) );
+					} else {
+						echo 'Sé el primero en comentar.';
+					}
 					?>
 				</ul>
-			</div><?php
-			else :
-				echo '<div class="six columns offset-by-one">No hay comentarios.</div>';
-			endif;
-
-
-			if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-			<div class="six columns offset-by-one">
-				<?php _e( 'Comments are closed.' ); ?>
-			</div><?php
-			endif; ?>
+			</div>
 		</div>
+
+
 	</div>
-</div>
+</div><?php
+
+endif; ?>
