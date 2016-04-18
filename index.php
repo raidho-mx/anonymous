ab58c82aefb433321aa847a07f0ddc5f36e5f7bc
raidho-mx <miranda@raidho.mx>
2016-04-18 18:50:12 -0500

    No results found

--- index.php
+++ index.php
@@ -11,7 +11,12 @@ Template Name: Reciente
 
 
 		if(is_search()) {
-			echo '<h2>Resultados para: '.get_search_query().'</h2>';
+			if(have_posts()) {
+				echo '<h2>Resultados para: '.get_search_query().'</h2>';
+			} else {
+				echo '<h2>No encontramos nada relacionado a <span class="rojo_txt">'.get_search_query().'</span>.</h2>';
+				$noSearch = TRUE;
+			}
 		} elseif(is_tag()) { ?>
 			<h2><?php single_tag_title('Etiqueta: '); ?></h2><?php
 		} elseif(is_archive()) { ?>
@@ -34,6 +39,23 @@ Template Name: Reciente
 			</div><?php
 
 			get_template_part('inc/pager');
+
+		else :
+
+			// If Empty Search Results
+			$allPosts = new WP_Query('post_type=post&posts_per_page=8');
+
+			if ( $allPosts->have_posts() ) : ?>
+				<h3>Aquí hay unos posts recientes:</h3>
+				<div class="row fluid"><?php
+					while ( $allPosts->have_posts() ) {
+						$allPosts->the_post();
+						cards(4);
+					} ?>
+				</div><?php
+			endif;
+			wp_reset_query();
+
 		endif;
 
 
