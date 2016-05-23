969c47d26d638b9900b5a3c3b4cfd5bfb5ce3cc8
raidho-mx <miranda@raidho.mx>
2016-05-23 09:21:41 -0500

    Updated Google Analytics

--- footer.php
+++ footer.php
@@ -100,6 +100,7 @@ wp_footer(); ?>
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/scripts/jquery.isonscreen.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/scripts/scripts.js"></script><?php
 
 if(is_single()) { ?>
@@ -126,12 +127,13 @@ if(is_single()) { ?>
 
 <?php // Remove while Staging. ?>
 <script>
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-	e.src='https://www.google-analytics.com/analytics.js';
-	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-	ga('create','UA-2607823-6','auto');ga('send','pageview');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-2607823-6', 'auto');
+	ga('send', 'pageview');
 </script>
 </body>
 </html>
