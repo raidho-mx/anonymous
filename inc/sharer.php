fb8b58458ba447dd622cf804f52805b02f8e754c
raidho-mx <miranda@raidho.mx>
2017-01-03 20:23:32 -0600

    Better twitter

--- inc/sharer.php
+++ inc/sharer.php
@@ -8,7 +8,7 @@ $fbSharer = 'https://www.facebook.com/sharer/sharer.php?s=100&p[url]='. urlencod
 <div class="post_sharer">
 	<p>Comparte este artículo:</p>
 	<ul class="follow_links">
-		<li class="follow_twitter"><a href="http://twitter.com/home?status=<?php echo wp_get_shortlink().': '; the_title(); ?> @_designaholic" target="_blank">Tw</a></li><li class="follow_google">
+		<li class="follow_twitter"><a href="http://twitter.com/home?status=<?php the_title(); ?> @_designaholic <?php echo wp_get_shortlink(); ?>" target="_blank">Tw</a></li><li class="follow_google">
 			<a href="https://plus.google.com/share?url=<?php echo urlencode($link); ?>">G+</a></li><li class="follow_facebook">
 			<a href="<?php echo $fbSharer; ?>" target="_blank">Fb</a></li><li class="follow_linkedin">
 			<!-- <a href="http://feeds.feedburner.com/designaholic/mx<?php // echo esc_url( home_url( '/feed/' ) ); ?>" target="_blank">Rss</a></li><li class="follow_pinterest"> -->
