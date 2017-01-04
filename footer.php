634484d032eca31a9659d63030a426f2b80d4f2f
raidho-mx <miranda@raidho.mx>
2017-01-03 20:15:47 -0600

    updated mailchimps

--- footer.php
+++ footer.php
@@ -19,12 +19,13 @@ wp_footer(); ?>
 				<h4>Suscríbete a nuestro newsletter</h4>
 
 				<div class="newsLetterContain">
-					<form id="mensual" action="//designaholic.us4.list-manage.com/subscribe/post?u=24f3f360c7d050182748142ae&amp;id=7435e069c4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+					<form id="mensual" action="//designaholic.us4.list-manage.com/subscribe/post?u=1a671e0d1692072eee0f68325&amp;id=eefa7bdd05" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 						<div id="mc_embed_signup_scroll">
-							<input type="text" value="" name="NOMBRE" class="required" id="mce-NOMBRE" placeholder="Tu Nombre">
-							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Tu Correo" required>
+							<input type="text" value="" name="FNAME" class="required" id="mce-FNAME" placeholder="Tu Nombre">
+							<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Tu Correo" required>
 							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_24f3f360c7d050182748142ae_7435e069c4" tabindex="-1" value=""></div>
+							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1a671e0d1692072eee0f68325_eefa7bdd05" tabindex="-1" value=""></div>
 							<div class="clear"><input type="submit" value="Suscríbete" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 						</div>
 					</form>
@@ -100,7 +101,6 @@ wp_footer(); ?>
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/scripts/jquery.isonscreen.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/scripts/scripts.js"></script><?php
 
 if(is_single()) { ?>
