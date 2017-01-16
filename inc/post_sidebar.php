0ef81632c1274ceffa474e474029d40834121a21
raidho-mx <miranda@raidho.mx>
2017-01-16 16:24:30 -0600

    Articles: Random from all tiers

--- inc/post_sidebar.php
+++ inc/post_sidebar.php
@@ -8,18 +8,51 @@ Single Post Sidebar
 <!-- Sidebar Single Post -->
 <div class="sidebar_post three columns offset-by-one"><?php
 
-
-
-	$rows = get_field('img_ads', 'option');
-	if($rows) : ?>
-		<h3>Publicidad</h3><?php
-
-		shuffle($rows);
-		$i = 0;
-		foreach($rows as $row) {
-			adSingleImg($row);
-			if (++$i == 1) break;
+	function checkActive($ads) {
+		$arr = array();
+		if($ads) {
+			foreach($ads as $row) {
+				if($row['active'] == 1) {	// Is active.
+					if($row['squares'][0]['img'] != '') { 	// Has image.
+						array_push($arr, $row);
+					}
+				}
+			}
 		}
+		return $arr;
+	}
+
+	$basic_ads = get_field('basic_ads', 'option');
+	$home_ads = get_field('home_ads', 'option');
+	$dh_ads = get_field('dh_ads', 'option');
+	$cat_ads = get_field('cat_ads', 'option');
+
+	$checkBasic = checkActive($basic_ads);
+	$checkHome = checkActive($home_ads);
+	$checkDH = checkActive($dh_ads);
+	$checkCat = checkActive($cat_ads);
+
+	$allChecked = array_merge($checkBasic, $checkHome, $checkDH, $checkCat);
+	$lucky = array_rand($allChecked, 1);
+	$luckyAd = $allChecked[$lucky];
+
+	if($luckyAd['squares'][0]['url']) {
+		$url = $luckyAd['squares'][0]['url'];
+	} elseif($luckyAd['url']) {
+		$url = $luckyAd['url'];
+	} else {
+		$url = '#';
+	}
+
+	if($luckyAd) : ?>
+		<h3>Publicidad</h3>
+		<div class="ad_group">
+			<div class="ad_google_uno">
+				<a href="<?php echo $url; ?>"<?php if($stripedId) echo ' id="'.$stripedId.'" data-title="'.$rawId.'"'; ?> class="register_ga" title="<?php echo $row['description']; ?>" onClick="ga('send', 'event', 'imagen: <?php echo $rawId; ?>', 'click', location.pathname);" target="_blank">
+					<img src="<?php echo $luckyAd['squares'][0]['img']; ?>" width="300" alt="<?php echo $luckyAd['squares'][0]['description']; ?>">
+				</a>
+			</div>
+		</div><?php
 	endif;
 
 
