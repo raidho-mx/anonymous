0b5e1faf7ea87166e559bd88d041229a69a19e3a
raidho-mx <miranda@raidho.mx>
2017-01-18 08:37:39 -0600

    Added Ads at slide down menu.

--- inc/top_slide_out_menu.php
+++ inc/top_slide_out_menu.php
@@ -1,8 +1,15 @@
 <?php
 /*
-Designaholic Templates
-Menú (superior) desplegable para categorías
-*/
+ *	Designaholic Templates
+ *	Menú (superior) desplegable para categorías
+ */
+
+	// Get Active ads & make a list
+	$cat_ads = get_field('cat_ads', 'option');
+	$checkCat = checkActiveStripes($cat_ads);
+	// print_r($cat_ads);
+	// echo '--------';
+	// print_r($checkCat);
 
 if(get_field('secondary_menu', 'options')) : ?>
 
@@ -11,16 +18,30 @@ if(get_field('secondary_menu', 'options')) : ?>
 		<ul class="cbp-hsmenu"><?php
 		while (have_rows('secondary_menu', 'options')) :
 			the_row();
-			$term = get_sub_field('page_link'); ?>
+			$term = get_sub_field('page_link');
+			$hostedCat = search($checkCat, 'tax_rel', $term->term_id); ?>
 			<li>
 				<a href="#"><?php echo $term->name; ?></a><?php
 
-
-				$taxQ = new WP_Query( array( 'category_name' => $term->slug, 'posts_per_page' => 4 ) );
+				if(!empty($hostedCat)) {
+					$lucky = array_rand($hostedCat, 1);
+					$luckyAd = $hostedCat[$lucky];
+					$ppp = 3;
+				} else {
+					$ppp = 4;
+				}
+				$taxQ = new WP_Query( array( 'category_name' => $term->slug, 'posts_per_page' => $ppp ) );
 
 				if ( $taxQ->have_posts() ) : ?>
 
 				<ul class="cbp-hssubmenu row fluid"><?php
+					if(!empty($hostedCat)) { ?>
+						<li class="three columns articulo">
+							<a href="<?php the_permalink(); ?>">
+								<img src="<?php echo $luckyAd['squares'][0]['img']; ?>">
+							</a>
+						</li><?php
+					}
 					while ( $taxQ->have_posts() ) {
 						$taxQ->the_post();
 						$class = checkClass();
